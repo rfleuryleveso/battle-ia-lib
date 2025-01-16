e79bae1048312a4ba740d5bd41aa136730817644
Ugo WAREMBOURG <112778362+UgoW@users.noreply.github.com>
2025-01-16 08:49:09 +0100

    Update build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-# Nom de l'image Docker
 IMAGE_NAME="battle_bot"
 
-# Chemin du Dockerfile (ajustez si nécessaire)
 DOCKERFILE="Dockerfile"
 
-# Construction de l'image Docker
-echo "Construction de l'image Docker..."
+echo "Building the Docker image..."
 docker build -t $IMAGE_NAME -f $DOCKERFILE .
 
-# Vérification du succès de la construction
 if [ $? -ne 0 ]; then
-    echo "Erreur : La construction de l'image Docker a échoué."
+    echo "Error: Docker image build failed."
     exit 1
 fi
 
-echo "Image $IMAGE_NAME construite avec succès."
+echo "Image $IMAGE_NAME built successfully."
 
-# Exécution du conteneur Docker
-echo "Exécution du conteneur Docker..."
+echo "Running the Docker container..."
 docker run $IMAGE_NAME
 
-# Vérification de l'exécution
 if [ $? -ne 0 ]; then
-    echo "Erreur : L'exécution du conteneur Docker a échoué."
+    echo "Error: Docker container run failed."
     exit 1
 fi
 
-echo "Conteneur $IMAGE_NAME exécuté avec succès."
+echo "Container $IMAGE_NAME ran successfully."

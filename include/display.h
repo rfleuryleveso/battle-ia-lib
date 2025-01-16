001878e1a2a2076affa656ff8637dfd33c4dd197
UgoW <ugo.warembourg@student.junia.com>
2025-01-16 10:39:44 +0100

    feat(display) - update display settings and add display functions

---
+++ include/display.h
@@ -0,0 +1,5 @@
+#include "battle_c.h"
+
+void displayMapDataObjects(BC_List *mapObjects);
+void displayMap(BC_List *mapObjects);
+void displayPlayerData(BC_PlayerData playerData);
--- src/header/display.h
+++
@@ -1,5 +0,0 @@
-// #include "battle_c.h"
-
-// void displayMapDataObjects(BC_List *mapObjects);
-// // void displayMap(BC_List *mapObjects);
-// // void displayPlayerData(BC_PlayerData playerData);
